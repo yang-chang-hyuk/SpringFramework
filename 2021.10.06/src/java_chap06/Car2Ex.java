c67d61c99e299417d966ed583422823e2b3fb83b
양창혁 <82005294+yang-chang-hyuk@users.noreply.github.com>
2021-10-06 12:00:08 +0900

    클래스의 멤버인 필드, 메서드

---
+++ 2021.10.06/src/java_chap06/Car2Ex.java
@@ -0,0 +1,16 @@
+package java_chap06;
+
+public class Car2Ex {
+
+	public static void main(String[] args) {
+		Car2 myCar = new Car2("포르쉐");
+		Car2 yourCar = new Car2("벤츠");
+		
+//		myCar.printSpeed();
+		
+		myCar.run();
+		yourCar.run();
+
+	}
+
+}
