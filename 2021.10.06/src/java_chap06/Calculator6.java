b81c4a43f3da2fd267549f006111d382d9ddf6c1
양창혁 <82005294+yang-chang-hyuk@users.noreply.github.com>
2021-10-06 09:51:39 +0900

    정사각형, 직사각형 넓이 구하기

---
+++ 2021.10.06/src/java_chap06/Calculator6.java
@@ -0,0 +1,14 @@
+package java_chap06;
+
+public class Calculator6 {
+//	정사각형의 넓이 출력
+	double areaRectangle(double width) {
+		return width * width;
+	}
+	
+//	직사각형의 넓이 출력
+	double areaRectangle(double width, double height) {
+		return width * height;
+	}
+
+}
