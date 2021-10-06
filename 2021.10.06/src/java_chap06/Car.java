32b694cf0138fa8f7e5106e85fca006b87639961
양창혁 <82005294+yang-chang-hyuk@users.noreply.github.com>
2021-10-06 10:20:45 +0900

    Car 라는 클래스를 생성

---
+++ 2021.10.06/src/java_chap06/Car.java
@@ -0,0 +1,13 @@
+package java_chap06;
+
+//	Car 라는 클래스를 생성
+public class Car {
+//	멤버 변수만 가지고 있는 클래스
+//	Car 클래스의 멤버 변수에 값을 기본적으로 지정해 놓았기 떄문에 Car 클래스를 이용하여 객체를 여러개 생성하여도 지정된 데이터가 모두 동일하게 포함되어 있음
+	String company = "현대 자동차";
+	String model = "그랜저";
+	String color = "검정";
+	int maxSpeed = 250;
+	int speed;
+
+}
