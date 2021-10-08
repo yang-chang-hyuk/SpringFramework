10a63187f64a9aefee14d34d7301ec7786e1baf2
양창혁 <82005294+yang-chang-hyuk@users.noreply.github.com>
2021-10-08 10:41:15 +0900

    Vehicle 클래스 타입의 변수 vehicle를 통해서 메서드 run()을 호출

---
+++ 2021.10.08/src/java_chap07/Driver.java
@@ -0,0 +1,9 @@
+package java_chap07;
+
+public class Driver {
+//	매개변수로 Vehicle 클래스 타입의 변수를 받음
+	public void drive(Vehicle vehicle) {
+//		Vehicle 클래스 타입의 변수 vehicle를 통해서 메서드 run()을 호출
+		vehicle.run();
+	}
+}
