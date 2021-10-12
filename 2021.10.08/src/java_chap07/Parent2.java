0d3b7ea7e31130895a00dd74cc7cc4cda681aa16
양창혁 <82005294+yang-chang-hyuk@users.noreply.github.com>
2021-10-12 11:38:33 +0900

    오버라이드

---
+++ 2021.10.08/src/java_chap07/Parent2.java
@@ -0,0 +1,11 @@
+package java_chap07;
+
+public class Parent2 {
+	void method1() {
+		System.out.println("Parent 클래스의 method1() 호출");
+	}
+	
+	void method2() {
+		System.out.println("Parent 클래스의 method2() 호출");
+	}
+}
